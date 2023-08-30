515257940cdfebee4e98641969e24c4b0b47e29c
sazarcode <cerberusprogrammer@gmail.com>
2023-08-29 17:09:34 -0700

    API implemented.

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content_md = models.TextField()  # Markdown content
     publication_date = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=24)
+    category = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
